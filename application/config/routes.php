ccfadccfa4b1f04bed10c3aba1dfd2948110d881
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-06-03 11:56:48 -0400

    up fix url amigavel

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'inicio';
 $route['404_override'] = 'error404';
-$route['translate_uri_dashes'] = TRUE;
-$route['perfil/(:id_usuario)'] = 'perfil/$1';
+$route['translate_uri_dashes'] = FALSE;
+$route['perfil/index/(:id_usuario)'] = 'perfil/$1';
