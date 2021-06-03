02b961b092750d75a63a42f4aa123dd6e28bf649
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-06-03 11:41:10 -0400

    route url amigavel

--- application/controllers/Perfil.php
+++ application/controllers/Perfil.php
@@ -15,9 +15,9 @@ class Perfil extends CI_Controller {
         $this->load->helper(array('form', 'url', 'html', 'directory'));
     }
 
-    public function index($id_usuario)
+    public function index()
     {
-        //$id_usuario = $this->input->get_post('id_usuario');
+        $id_usuario = $this->input->get_post('id_usuario');
 
         $data = array(
             'titulo' => 'BuscaJobs - Os melhores profissionais, você encontra aqui!',
