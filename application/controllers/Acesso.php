df0964ec7971962b53ff4f3d41ddbda9682f3044
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-06-03 22:08:43 -0400

    up

--- application/controllers/Acesso.php
+++ application/controllers/Acesso.php
@@ -34,25 +34,27 @@ class Acesso extends CI_Controller {
         if(!isset($dados_acesso[0]["mensagem"])):
             $this->session->set_userdata('log_hash_acesso',$dados_acesso[0]['hash_acesso']);
 
-            $data = array(
-                'titulo'    => 'Login - BuscaJobs',
-                'lista'     => $this->permissoes->init_permissao($this->session->userdata('log_hash_acesso'))
-                //'mensagem'  => $dados_acesso[0]["mensagem"]
-            );
-            $this->load->view('login',$data);
+            print_r($dados_acesso);
 
-            //redirect('login', 'refresh');
+            // $data = array(
+            //     'titulo'    => 'Login - BuscaJobs',
+            //     'lista'     => $this->permissoes->init_permissao($this->session->userdata('log_hash_acesso'))
+            //     //'mensagem'  => $dados_acesso[0]["mensagem"]
+            // );
+            //$this->load->view('login',$data);
+
+            // redirect('login', 'refresh');
         // else:
             //$this->permissoes->init_session(), // nao existe
             //redirect('login','refresh');
         endif;
 
-            $data = array(
-                'titulo'    => 'Login - BuscaJobs',
-                'lista'     => $this->permissoes->init_permissao($this->session->userdata('log_hash_acesso'))
-                //'mensagem'  => $dados_acesso[0]["mensagem"]
-            );
-            $this->load->view('login',$data);
+            // $data = array(
+            //     'titulo'    => 'Login - BuscaJobs',
+            //     'lista'     => $this->permissoes->init_permissao($this->session->userdata('log_hash_acesso'))
+            //     //'mensagem'  => $dados_acesso[0]["mensagem"]
+            // );
+            // $this->load->view('login',$data);
 
     }
     
