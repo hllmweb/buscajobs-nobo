d0dab90e242d0b7bb7eb6ae7be2fabd8a8fb4e35
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-06-03 20:08:54 -0400

    auth acesso

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -19,7 +19,7 @@ class Login extends CI_Controller {
 	{
 
 
-		if(isset($this->session->userdata('log_hash_acesso'))){
+		if(!isset($this->session->userdata('log_hash_acesso'))){
 
 		$dados_acesso = $this->acesso->auth(array(
 	                                            'p_operacao'  => 'CHECK_PERMISSAO',
