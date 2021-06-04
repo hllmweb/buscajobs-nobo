21ab4c62d561d6c7cad01300d6bccc641d3f42f0
Hugo Mesquita <hugomesquitaweb@gmail.com>
2021-06-03 22:31:32 -0400

    up

--- application/controllers/Cadastrar.php
+++ application/controllers/Cadastrar.php
@@ -3,10 +3,23 @@ defined('BASEPATH') OR exit('Não é permitido acesso direto');
 
 class Cadastrar extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+
+		//models
+		// $this->load->model('M_Filter','filter');
+
+		//libs
+		$this->load->library(array('session','permissoes'));
+		$this->load->helper(array('form', 'url', 'html', 'directory','file'));
+
+	}
+
 	public function index()
 	{
 		$data = array(
-			'titulo' => 'BuscaJobs - Cadastre-se e encontre os melhores profissionais'
+			'titulo' => 'BuscaJobs - Cadastre-se e encontre os melhores profissionais',
+			'lista' 			=>  $this->permissoes->init_permissao($this->session->userdata('log_hash_acesso'))
 		);
 
 		$this->load->view('cadastrar/index', $data);
